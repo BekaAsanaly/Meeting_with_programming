2e78592c0dadd4403cc746cba35f85724e89f70b
beka <beqool.3@gmail.com>
2023-09-15 21:20:05 +0600

    IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "jon")
+{
+    Console.WriteLine("Hura, This is jon!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
